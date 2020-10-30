2764d32b32559202ca6f52b5936f21785bf5a039
nakanomuramoto <nakano.muramoto@gmail.com>
2020-10-30 11:19:51 +0900

    Add files via upload

--- TestCVp59.py
+++ TestCVp59.py
@@ -1,13 +1,20 @@
 from scipy import ndimage
+from PIL import Image
 
 import numpy as np
-import matplotlib.python as plt
+import matplotlib.pyplot as plt
 
 im = np.array(Image.open('empire.jpg').convert('L'))
-H = np.array([[1.4, 0.05, -100], [0.05, 1.5, -100], [0, 0, 1]])
-im2 = ndimage.affine_transport(im, [:2, :2], (H[0, 2], H[1, 2]))
+H = np.array([[1.4, 0.05, -100], [0.05, 1.5, -150], [0, 0, 1]])
+im2 = ndimage.affine_transform(im, H[:2, :2], (H[0, 2], H[1, 2]))
+#im2 = ndimage.affine_transform(im, H[:2, :2])
+
+print("H[0, 2] : ", H[0, 2])
+print("\nH[1, 2] : ", H[1, 2])
 
 plt.figure()
 plt.gray()
+plt.imshow(im)
+plt.show()
 plt.imshow(im2)
 plt.show()
